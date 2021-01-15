bc34a6306cec683f30d54cc09dfcf4e91d4708da
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-01-15 15:49:14 -0800

    add various functionality.

--- rpg-app/src/render.js
+++ rpg-app/src/render.js
@@ -3,19 +3,35 @@ const beginGame = () => {
     begin.classList.add('hide');
     renderUserSetup()
 }
+const fs = require('fs');
 const renderUserSetup = () => {
     let name = document.createElement('h3');
     name.innerHTML = 'Name';
     let userNameInput = document.createElement('input');
     let jobList = document.createElement('select');
     let jobChoices = ['wizard', 'rogue', 'warrior'];
-    document.body.append(name, userNameInput, jobList);
+    let userSubmitBtn = document.createElement('button');
+    userSubmitBtn.innerHTML = "Ready!";
+    userSubmitBtn.setAttribute('id', 'ready');
+    document.body.append(name, userNameInput, jobList, userSubmitBtn);
     jobChoices.forEach(function(job){
         let choice = document.createElement('option');
         choice.setAttribute('value', job)
         choice.innerHTML = `${job}`;
         jobList.append(choice);
     })
+
+
 }
 
-begin.addEventListener('click', beginGame)
+const writeInfo = () => {
+    fs.writeFile('playerProfile.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+}
+
+const ready = document.getElementById('ready');
+
+begin.addEventListener('click', beginGame);
+ready.addEventListener('click', writeInfo);
